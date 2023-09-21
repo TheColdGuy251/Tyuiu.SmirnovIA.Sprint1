40355987d2ad1da330c6892fd3f3d887063ec31c
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-09-21 18:11:52 +0500

    Task3

---
+++ Tyuiu.SmirnovIA.Sprint1.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SmirnovIA.Sprint1.Task3.V10.Lib;
+
+namespace Tyuiu.SmirnovIA.Sprint1.Task3.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 10.6;
+            var res = ds.NumberToMoney(x);
+            Assert.AreEqual(10.6, res);
+        }
+    }
+}
---
+++ Tyuiu.SmirnovIA.Sprint1.Task3.V10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SmirnovIA.Sprint1.Task3.V10.Lib;
+
+namespace Tyuiu.SmirnovIA.Sprint1.Task3.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Смирнов И. А. | ИИПб-23-3";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Смирнов Илья Алексеевич | ИИПб-23-3");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите дробное число");
+            double x = Convert.ToDouble(Console.ReadLine());
+            ds.NumberToMoney(x);
+            string str = Convert.ToString(x);
+            string[] money = str.Split(',');
+
+            string lowMoney = money.Length < 2 ? "00" : money[1].Length > 1 ? money[1] : money[1] + "0";
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"{x} руб. — это {money[0]} руб. {lowMoney} коп.");
+            Console.ReadKey();
+        }
+    }
+}
