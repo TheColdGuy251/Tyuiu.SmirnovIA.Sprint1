d17ea55494aa12416ee120f93f21a08cc10cdffb
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-09-28 17:22:33 +0500

    Task7

---
+++ Tyuiu.SmirnovIA.Sprint1.Task7.V17.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.SmirnovIA.Sprint1.Task7.V17.Lib
+{
+    public class DataService : ISprint1Task7V17
+    {
+        public double Calculate(double x, double y)
+        {
+            double res = Math.Round((1 + Math.Sin(Math.Sqrt(x * x + 1))) / (Math.Cos(12 * y - 4)), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SmirnovIA.Sprint1.Task7.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SmirnovIA.Sprint1.Task7.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SmirnovIA.Sprint1.Task7.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d867911c-b211-4605-a618-6209ddc5801e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
